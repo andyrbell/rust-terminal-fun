19a44295237709e912f483ff1e2336c37b7b12a0
Andy Bell <andyrbell@gmail.com>
2019-10-15 23:48:56 +0100

    Cli args

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,22 @@ extern crate chrono;
 
 #[allow(unused_imports)]
 use crate::docker::hello_docker;
+use structopt::StructOpt;
 
 pub mod docker;
 pub mod github;
 pub mod ui; 
-pub mod devoxx; 
+pub mod devoxx;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rust-web", about = "A command line tool")]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool
+}
 
 fn main() -> Result<(), failure::Error> {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
     ui::repos_screen::run()
 }
